62571a02d9fb437cd56f3fb7b5903ba480305522
Sagar Shiroya <sagar.shiroya8@gmail.com>
2021-12-27 18:55:30 +0530

    Removing commented code from .js file

--- index.js
+++ index.js
@@ -11,9 +11,6 @@ function fetchCountry() {
         if(xhr.status == 200){
             console.log('Success');
             let countries = JSON.parse(this.response);
-            // console.log(countries);
-            // console.log(Array.isArray(JSON.parse(countries)));
-            // console.log(typeof JSON.parse(countries));
             countries.forEach(fetchCountryName)
         }
     };
@@ -22,11 +19,8 @@ function fetchCountry() {
 }
 
 function fetchCountryName(country) {
-    // console.log(country['name']['common']);
-    // console.log(country['flags']['png']);
 
     const countryDiv = document.createElement('div');
-    countryDiv
     
     const countryName =document.createElement('p');
     countryName.innerHTML = country['name']['common'];
@@ -36,5 +30,6 @@ function fetchCountryName(country) {
 
     countryDiv.appendChild(countryName);
     countryDiv.appendChild(countryFlag);
+    
     document.getElementById('feed').appendChild(countryDiv);
 }
